370cecb453ef90f04417ec4c9f7cca0a3f03b7a0
TMarkKI <117105171+TMarkKI@users.noreply.github.com>
2024-09-12 11:58:21 +0100

    Update graphing_annovar.py
    Added a discrepancy function to find discrepancies in the SNPs called based on genotype from variant caller.
    
    Added colours to plotting function to reflect the discrepancies.

--- graphing_annovar.py
+++ graphing_annovar.py
@@ -4,6 +4,7 @@ import subprocess
 import glob
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Try importing r, install it if not found
 #try:
@@ -31,13 +32,55 @@ class ScriptException(Exception):
             self.stderr = stderr
             Exception().__init__('Error in annovar script')
 
+#process annotated table to seek out discrepancies in the samples; uses positional/column options so this will affect the columns specific to the number of samples
+def discrepancy_finder(table):
+    df = pd.read_csv(table, sep='\t', header=None)
+     
+    #conditional insertion for comparison between the genotypes of two samples
+    df['16h_genotype_score'] = np.where(df[5] == '{0/0}', 0,
+                                        np.where(df[5] == '{0/1}', 1.5, 
+                                                 np.where(df[5] == '{1/1}', 1, np.nan)))
+
+    df['8h_genotype_score'] = np.where(df[11] == '{0/0}', 0,
+                                       np.where(df[11] == '{0/1}', 1.5, 
+                                                np.where(df[11] == '{1/1}', 1, np.nan)))
+
+    df['discrepant score'] = df['16h genotype score'] + df['8h genotype score'] #need to tabulate score to assign condition of gt
+
+    df['condition'] = np.where(df['discrepant_score'] == 0, 'wild-type',
+                               np.where(df['discrepant_score'] == 1.5, 'wild-type and mixed',
+                                        np.where(df['discrepant_score'] == 3, 'mixed',
+                                                 np.where(df['discrepant_score'] == 2.5, 'alternate and mixed',
+                                                          np.where(df['discrepant_score'] == 2, 'alternate', 
+                                                                   'alternate and wild-type')))))
+    return df
+
+
+
 #function to graph the results into R graph
 def plotfunc(filename, output_dir):
-    df = pd.read_csv(filename, sep='\t', header=None, usecols=[0,1], names=['Chromosome', ['Position']])
-    df['Chromosome'] = df['Chromosome'].astype(str) #converts chromosome into strings for categorical data (i.e. chromosome 1 will be 1)
+    df = pd.read_csv(filename, sep='\t')
+    df_plot = discrepancy_finder(df)
+    #df = pd.read_csv(filename, sep='\t', header=None, usecols=[0,1,15], names=['Chromosome', 'Position','condition'])
+    #df['Chromosome'] = df['Chromosome'].astype(str) #converts chromosome into strings for categorical data (i.e. chromosome 1 will be 1)
 
-    #columns chrom and pos
-    plot = df.plot(kind = 'scatter', x = 'Chromosome', y = 'Position', title=filename)
+    #columns chrom and pos; added another section to determine colour based on discrepancy/acceptancy
+    #colour conditions
+    conditions_colour_map = {
+        'wild-type': 'blue',
+        'wild-type and mixed': 'green',
+        'mixed': 'yellow',
+        'alternate and mixed': 'orange',
+        'alternate': 'red',
+        'alternate and wild-type': 'purple'
+    }
+
+    df_plot['colour'] = df_plot['condition'].map(conditions_colour_map)
+    plot = df_plot.plot(kind='scatter', x='Chromosome', y='Position', color=df_plot['colour'], title=filename)
+
+
+    #df_plot = pd.read_csv(filename, sep='\t', header=None, usecols=[0,1,15], names=['Chromosome', 'Position', 'condition'])
+    #plot = df_plot.plot(kind = 'scatter', x = 'Chromosome', y = 'Position', title=filename)
     
     #save plot to output directory
     prefix_plot = os.path.join(output_dir, os.path.basename(filename).replace(".tab", "_plot.jpg"))
@@ -46,6 +89,8 @@ def plotfunc(filename, output_dir):
     plt.close()
 
 
+
+#main section of code
 def main(input_directory, output_dir):
     #debug output directory
     print(f"Output directory: {output_dir}")
@@ -55,8 +100,8 @@ def main(input_directory, output_dir):
     
     #create annotated files
     script = "/mnt/storage8/mtan/scripts/annotate.sh" #to change where bash script annotate.sh is stored
-    input_path = [os.path.join(input_directory)]
-    os.chdir(input_path) #changing into the directory where the vcf files are there
+    #input_path = [os.path.join(input_directory)]
+    os.chdir(input_directory) #changing into the directory where the vcf files are there
 
     try:
         stdout, stderr = run_script_annovar(script)
@@ -76,7 +121,7 @@ def main(input_directory, output_dir):
     else:
          for tab_file in tab_directory:
             print(f"Processing {tab_file}")
-            plot = plotfunc(os.path.join(annotation_path, tab_file))
+            plotfunc(os.path.join(annotation_path, tab_file), output_dir)
     
     
 
